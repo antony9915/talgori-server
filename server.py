170c84886fdb4767a48c61b2b4f808a88875031b
antony9915 <aron9915mozocuri@gmail.com>
2026-05-29 22:19:37 -0500

    update url

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 import time
@@ -75,11 +76,11 @@ def get_signal(token: str):
 # =====================================================
 
 if __name__ == "__main__":
+
     import uvicorn
+
     uvicorn.run(
         app,
         host="0.0.0.0",
         port=8000
     )
-
-    
