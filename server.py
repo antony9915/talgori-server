68322b7b0df121eaf679107e4c9de9461fc604ae
antony9915 <aron9915mozocuri@gmail.com>
2026-05-29 21:40:08 -0500

    update url

--- server.py
+++ server.py
@@ -75,12 +75,11 @@ def get_signal(token: str):
 # =====================================================
 
 if __name__ == "__main__":
-
     import uvicorn
-
     uvicorn.run(
         app,
         host="0.0.0.0",
         port=8000
     )
 
+    
